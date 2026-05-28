5c77685072bb3bbd69694fb037b80cae57d7bf88
harshikashri <harshikashribalaraja@gmail.com>
2026-05-28 14:08:19 +0530

    updated

--- Dockerfile
+++ Dockerfile
@@ -14,6 +14,8 @@ FROM nginx:alpine
 
 COPY --from=builder /app/dist /usr/share/nginx/html
 
+RUN sed -i 's/listen       80;/listen 8080;/g' /etc/nginx/conf.d/default.conf
+
 EXPOSE 8080
 
 CMD ["nginx", "-g", "daemon off;"]
--- src/app/config/api.ts
+++ src/app/config/api.ts
@@ -1,6 +1,6 @@
 export const API_CONFIG = {
-  authBaseUrl: import.meta.env.VITE_AUTH_API_URL ?? "http://localhost:8000",
-  bookingBaseUrl: import.meta.env.VITE_BOOKING_API_URL ?? "http://localhost:8001",
+  authBaseUrl: import.meta.env.VITE_AUTH_API_URL ?? "https://auth-backend-uvifs5ttza-el.a.run.app",
+  bookingBaseUrl: import.meta.env.VITE_BOOKING_API_URL ?? " https://booking-backend-uvifs5ttza-el.a.run.app",
 } as const;
 
 export const AUTH_ENDPOINTS = {
